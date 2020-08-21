5882edd1d1ce2918e223c7690db119c7c21d39a1
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-08-21 22:19:36 +0200

    0.2.0 - More modular structure: - run_comrad_sim_hpc now just runs jobs on hpc - catch_job_ids is used to catch job IDs on a call to run_comrad_sim_hpc (or another similar function) - model parameters are supplied through create_comrad_params, alla mbd

---
+++ R/create_comrad_params.R
@@ -0,0 +1,49 @@
+#' Create a list of standard comrad parameters
+#'
+#' Just returns a list with all standard parameters. This makes it easy to
+#' modify a few while keeping default values for others.
+#'
+#' @inheritParams default_params_doc
+#'
+#' @author Théo Pannetier
+#' @export
+create_comrad_params <- function(
+   competition_sd = comrad::default_competition_sd(),
+   carrying_cap_sd = comrad::default_carrying_cap_sd(),
+   carrying_cap_opt = comrad::default_carrying_cap_opt(),
+   trait_opt = comrad::default_trait_opt(),
+   growth_rate = comrad::default_growth_rate(),
+   prob_mutation = comrad::default_prob_mutation(),
+   mutation_sd = comrad::default_mutation_sd(),
+   trait_dist_sp = comrad::default_trait_dist_sp()
+) {
+   # Check params format
+   comrad::testarg_num(competition_sd)
+   comrad::testarg_pos(competition_sd)
+   comrad::testarg_num(carrying_cap_sd)
+   comrad::testarg_pos(carrying_cap_sd)
+   comrad::testarg_num(carrying_cap_opt)
+   comrad::testarg_pos(carrying_cap_opt)
+   comrad::testarg_num(trait_opt)
+   comrad::testarg_num(growth_rate)
+   comrad::testarg_pos(growth_rate)
+   comrad::testarg_num(prob_mutation)
+   comrad::testarg_prop(prob_mutation)
+   comrad::testarg_num(mutation_sd)
+   comrad::testarg_pos(mutation_sd)
+   comrad::testarg_num(trait_dist_sp)
+   comrad::testarg_pos(trait_dist_sp)
+
+   # Return params in a single list
+   comrad_params <- list(
+      "competition_sd" = competition_sd,
+      "carrying_cap_sd" = carrying_cap_sd,
+      "carrying_cap_opt" = carrying_cap_opt,
+      "trait_opt" = trait_opt,
+      "growth_rate" = growth_rate,
+      "prob_mutation" = prob_mutation,
+      "mutation_sd" = mutation_sd,
+      "trait_dist_sp" = trait_dist_sp
+   )
+   return(comrad_params)
+}
--- R/run_comrad_sim_hpc.R
+++ R/run_comrad_sim_hpc.R
@@ -3,11 +3,11 @@
 #' Calls `run_comrad_sim.bash` and passes arguments to
 #' [comrad::run_simulation()], submits the simulations through [ssh].
 #'
+#' @inheritParams default_params_doc
 #' @param nb_replicates numeric, number of replicate simulations to run. All
 #' replicates share the same parameters. One job is submitted per replicate.
-#' @inheritParams default_params_doc
-#' @param return_job_ids logical. By default, the job IDs are captured and
-#' returned.
+#' @param comrad_params a list of parameters for [comrad::run_simulation()],
+#' as created with [create_comrad_params()]
 #'
 #' @author Théo Pannetier
 #' @export
@@ -15,87 +15,46 @@
 run_comrad_sim_hpc <- function(
   nb_gens,
   nb_replicates = 1,
-  competition_sd = comrad::default_competition_sd(),
-  carrying_cap_sd = comrad::default_carrying_cap_sd(),
-  carrying_cap_opt = comrad::default_carrying_cap_opt(),
-  trait_opt = comrad::default_trait_opt(),
-  growth_rate = comrad::default_growth_rate(),
-  prob_mutation = comrad::default_prob_mutation(),
-  mutation_sd = comrad::default_mutation_sd(),
-  trait_dist_sp = comrad::default_trait_dist_sp(),
-  return_job_ids = TRUE
+  comrad_params = hamr::create_comrad_params()
 ) {
   # Check input
+  comrad::testarg_num(nb_gens)
+  comrad::testarg_pos(nb_gens)
+  comrad::testarg_not_this(nb_gens, 0)
   comrad::testarg_num(nb_replicates)
   comrad::testarg_int(nb_replicates)
   comrad::testarg_not_this(nb_replicates, 0)
-  comrad::testarg_num(nb_gens)
-  comrad::testarg_pos(nb_gens)
-  comrad::testarg_int(nb_gens)
-  comrad::testarg_num(competition_sd)
-  comrad::testarg_pos(competition_sd)
-  comrad::testarg_num(carrying_cap_sd)
-  comrad::testarg_pos(carrying_cap_sd)
-  comrad::testarg_num(carrying_cap_opt)
-  comrad::testarg_pos(carrying_cap_opt)
-  comrad::testarg_num(trait_opt)
-  comrad::testarg_num(growth_rate)
-  comrad::testarg_pos(growth_rate)
-  comrad::testarg_num(prob_mutation)
-  comrad::testarg_prop(prob_mutation)
-  comrad::testarg_num(mutation_sd)
-  comrad::testarg_pos(mutation_sd)
-  comrad::testarg_num(trait_dist_sp)
-  comrad::testarg_pos(trait_dist_sp)
-  comrad::testarg_log(return_job_ids)
 
   # Connect to hpc
   session <- ssh::ssh_connect(
     "p282688@peregrine.hpc.rug.nl"
   )
 
-  # Prepare command
+  # Concatenate command
   command <- paste(
     "sbatch comrad/scripts/bash/run_comrad_sim.bash",
     nb_gens,
-    competition_sd,
-    carrying_cap_sd,
-    carrying_cap_opt,
-    trait_opt,
-    growth_rate,
-    prob_mutation,
-    mutation_sd,
-    trait_dist_sp
+    comrad_params$competition_sd,
+    comrad_params$carrying_cap_sd,
+    comrad_params$carrying_cap_opt,
+    comrad_params$trait_opt,
+    comrad_params$growth_rate,
+    comrad_params$prob_mutation,
+    comrad_params$mutation_sd,
+    comrad_params$trait_dist_sp
   )
 
-  # For each replicate
-  job_ids <- purrr::map(
+  # Submit job nb_replicate times to hpc
+  purrr::walk(
     1:nb_replicates,
     function(x) {
-      # Capture output to extract job ID
-      console_output <- utils::capture.output(
-        # Submit job to hpc
-        ssh::ssh_exec_wait(
-          session = session,
-          command = command
-        ),
-        type = "output"
+      ssh::ssh_exec_wait(
+        session = session,
+        command = command
       )
-      # Extract job ID
-      job_id <- console_output %>%
-        stringr::str_extract("^Submitted batch job [:digit:]{8}$") %>%
-        stats::na.omit() %>%
-        stringr::str_extract("[:digit:]{8}$") %>%
-        as.numeric()
-      return(job_id)
     }
-  ) %>% unlist()
-
+  )
   ssh::ssh_disconnect(
     session = session
   )
-
-  if (return_job_ids) {
-    return(job_ids)
-  }
 }
