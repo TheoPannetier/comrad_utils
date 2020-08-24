817e107e876766cd1d4a9eecf5ea63d2a686d746
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-08-25 00:01:29 +0200

    renamed downlaod_logbook_hpc

--- R/download_logbook_hpc.R
+++ R/download_logbook_hpc.R
@@ -5,19 +5,18 @@
 #' @author Théo Pannetier
 #' @export
 #'
-download_logbook <- function() {
+download_logbook_hpc <- function() {
   # Connect to hpc
   session <- ssh::ssh_connect(
     "p282688@peregrine.hpc.rug.nl"
   )
   # Get ssh to download file
-
-      ssh::scp_download(
-        session = session,
-        files = "/data/$USER/fabrika/comrad_data/logs/logbook.csv",
-        to = "~/Github/fabrika/comrad_data/logs/"
-      )
-
+  ssh::scp_download(
+    session = session,
+    files = "/data/$USER/fabrika/comrad_data/logs/logbook.csv",
+    to = "~/Github/fabrika/comrad_data/logs/"
+  )
+  # Disconnect
   ssh::ssh_disconnect(
     session = session
   )
