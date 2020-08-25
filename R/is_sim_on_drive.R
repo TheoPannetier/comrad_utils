90c1d8db53ce82cc35fe05d680b196271976602c
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-08-25 17:32:46 +0200

    check if a sim file is present on drive

---
+++ R/is_sim_on_drive.R
@@ -0,0 +1,29 @@
+#' Check if a simulation file is present on Google Drive
+#'
+#' @param job_ids eight-digit job ID given by Peregrine upon
+#' submission.
+#'
+#' @author Théo Pannetier
+#'
+#' @name is_sim_on_drive
+NULL
+
+#' @rdname is_sim_on_drive
+#' @export
+is_sim_csv_on_drive <- function(job_ids) {
+  ls <- googledrive::drive_ls("comrad_data/sims/")
+  jobs_present <- ls$name %>%
+    stringr::str_match(pattern = "^comrad_sim_([:digit:]{8}).csv$") %>%
+    .[,2]
+  return(job_ids %in% jobs_present)
+}
+
+#' @rdname is_sim_on_drive
+#' @export
+is_sim_log_on_drive <- function(job_ids) {
+  ls <- googledrive::drive_ls("comrad_data/logs/")
+  jobs_present <- ls$name %>%
+    stringr::str_match(pattern = "^comrad_sim_([:digit:]{8}).log$") %>%
+    .[,2]
+  return(job_ids %in% jobs_present)
+}
