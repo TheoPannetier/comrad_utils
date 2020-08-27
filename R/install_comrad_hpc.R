2ccb1bfb6bc62151e0617ad68c878f507bf15660
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-08-27 14:11:52 +0200

    previous versions of comrad can be installed

--- R/install_comrad_hpc.R
+++ R/install_comrad_hpc.R
@@ -3,18 +3,26 @@
 #' Install or update the [comrad](https://github.com/TheoPannetier/comrad)
 #' package (from the `master` branch) to Peregrine.
 #'
+#' @param ref see [remotes::install_github()]
+#'
 #' @author Théo Pannetier
 #' @export
 #'
-install_comrad_hpc <- function() {
+install_comrad_hpc <- function(ref = "master") {
   # Connect to hpc
   session <- ssh::ssh_connect(
     "p282688@peregrine.hpc.rug.nl"
   )
+
+  command <- paste(
+    "sbatch /data/$USER/fabrika/bash/install_comrad.bash",
+    ref
+  )
+
   # Submit job to install comrad
   ssh::ssh_exec_wait(
     session = session,
-    command = "sbatch /data/$USER/fabrika/bash/install_comrad.bash"
+    command = command
   )
 
   ssh::ssh_disconnect(
--- bash/install_comrad.bash
+++ bash/install_comrad.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
-#SBATCH --output=install_comrad.log --partition=short --time=00:09:59
+#SBATCH --output=install_comrad.log --partition=short --time=00:04:59
 
 module load R
 
-Rscript -e "remotes::install_github(\"TheoPannetier/comrad\")"
+REF=${1:-master}
+
+Rscript -e "remotes::install_github(\"TheoPannetier/comrad\", ref=\"${REF}\"); packageVersion(\"comrad\")"
