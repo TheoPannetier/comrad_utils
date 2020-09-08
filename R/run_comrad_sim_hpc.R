83c9d43b959e982207e2079e851dd220c04ab8b7
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-09-08 13:37:59 +0200

    walltime and nb_generations now mapped on params_array – close #8

--- R/run_comrad_sim_hpc.R
+++ R/run_comrad_sim_hpc.R
@@ -4,7 +4,10 @@
 #' [comrad::run_simulation()], submits the simulation jobs through [ssh] to
 #' Peregrine.
 #'
-#' @inheritParams default_params_doc
+#' @param nb_gens integer, how many generations should the simulation be run
+#' for? Length must be either 1 (all jobs are given the same number of
+#' generations) or `nrow(params_array)` (custom value for each job).
+#' walltime for each job).
 #' @param params_array a data frame containing all combinations of `comrad`
 #' parameters to submit. One column per parameter, one row for each unique
 #' combination of values. Tip: use [create_comrad_params()] to provide the
@@ -25,7 +28,9 @@
 #' so simulations run with the same seed will be identical. Use this to repeat
 #' simulations, otherwise use the default.
 #' @param walltime character, maximum time allocated by the HPC to a job.
-#' Format must be either `HH:MM:SS` or `D-HH:MM:SS`.
+#' Format must be either `HH:MM:SS` or `D-HH:MM:SS`. Length must be either 1
+#' (all jobs are given the same walltime) or `nrow(params_array)` (custom
+#' walltime for each job).
 #' @param check_comrad_version logical. If `TRUE`, `fabrika` compares installed
 #' versions of `comrad` on HPC vs local before running the simulations, and
 #' return an error if they mismatch.
@@ -42,21 +47,35 @@ run_comrad_sim_hpc <- function(
   walltime = "00:57:00",
   check_comrad_version = TRUE
 ) {
-  # Check input
+
+  # Input control
+  if (!is.data.frame(params_array)) {
+    stop("argument \"params_array\" should be a data frame.")
+  }
+  exptd_var_names <- fabrika::create_comrad_params() %>% names()
+  if (!all(params_array %>% names() == exptd_var_names)) {
+    stop("\"params_array\" variables should be the same as in create_comrad_params()")
+  }
   comrad::testarg_num(nb_gens)
   comrad::testarg_pos(nb_gens)
+
   comrad::testarg_not_this(nb_gens, 0)
+  if (!length(nb_gens) %in% c(1, nrow(params_array))) {
+    stop("argument \"nb_gens\" must have length either 1 or nrow(params_array)")
+  }
   comrad::testarg_num(nb_replicates)
   comrad::testarg_int(nb_replicates)
   comrad::testarg_not_this(nb_replicates, 0)
   comrad::testarg_num(seeds)
   comrad::testarg_length(seeds, nb_replicates * nrow(params_array))
-
   is_walltime <- function (walltime) {
     stringr::str_detect(walltime, "^([0-9]-)?[0-9]{2}:[0-5][0-9]:[0-5][0-9]$")
   }
   if (!all(is_walltime(walltime))) {
     stop("argument \"walltime\" is not a walltime")
+  }
+  if (!length(walltime) %in% c(1, nrow(params_array))) {
+    stop("argument \"walltime\" must have length either 1 or nrow(params_array)")
   }
 
   # Check comrad version
@@ -68,13 +87,16 @@ run_comrad_sim_hpc <- function(
   batch_id <- paste0("b", sample(10000:99999, 1))
   cat("Jobs submitted with batch ID", batch_id, "\n")
 
+  # Concatenate sbatch calls
   params_array <- params_array %>% dplyr::mutate(
     "nb_gens" = nb_gens,
     "walltime" = walltime
   )
-
-  # Concatenate sbatch calls
   commands <- params_array %>%
+    dplyr::mutate(
+      "nb_gens" = nb_gens,
+      "walltime" = walltime
+    ) %>%
     glue::glue_data(
       "sbatch",
       "--time={walltime}",
@@ -109,6 +131,7 @@ run_comrad_sim_hpc <- function(
       command = command
     )
   })
+
   # Disconnect
   ssh::ssh_disconnect(
     session = session
