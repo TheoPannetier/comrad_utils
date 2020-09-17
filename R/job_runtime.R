65df320e847ae3c060a4fd3c594d97447e70d88c
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-09-17 16:30:53 +0200

    job_runtime should be returned even if the job hasn't completed

--- R/job_runtime.R
+++ R/job_runtime.R
@@ -13,14 +13,11 @@ job_runtime <- function(
 ) {
   # Capture jobinfo
   info <- utils::capture.output(fabrika::job_info(job_id = job_id))
-  # Extract runtime if job complete, otherwise NA
-  runtime <- ifelse(
-    has_job_completed(job_id = job_id),
-    stats::na.omit(stringr::str_match(
-      info,
-        "^Used walltime[:blank:]{7}\\:[:blank:]{1,3}([\\d+\\-]?\\d{2}\\:\\d{2}\\:\\d{2})$"
-    )[, 2]),
-    NA
+  # Extract runtime
+  runtime <- stats::na.omit(
+    stringr::str_match(
+      info, "^Used walltime[:blank:]{7}\\:[:blank:]{1,3}([\\d+\\-]?\\d{2}\\:\\d{2}\\:\\d{2})$"
+    )[, 2]
   )
   runtime <- hms::as_hms(runtime)
   return(runtime)
