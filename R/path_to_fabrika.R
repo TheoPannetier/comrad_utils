2a5663a2ed48af9f45c8b1c7a91ede0836f8680e
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-11-13 13:38:23 +0100

    in progress - write path to fabrika to rprofile

--- R/path_to_fabrika.R
+++ R/path_to_fabrika.R
@@ -11,3 +11,13 @@ path_to_fabrika_local <- function() {
 path_to_fabrika_hpc <- function() {
   "/data/$USER/fabrika/"
 }
+
+write_path_to_fabrika_local <- function(path_to_fabrika = here::here()) {
+
+  if (!stringr::str_sub(path_to_fabrika, start = -7L, end = -1L) == "fabrika") {
+    stop("\"path_to_fabrika\" does not point to fabrika. Try again from within fabrika.Rproj")
+  }
+  r_profile <- readr::read_file(paste0(path_to_fabrika, "/.Rprofile"))
+}
+
+path_to_fabrika <- here::here()
