2563904cc7be5e632efef15a7c7ad8274730b429
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-12-01 17:38:27 +0100

    copy files to ext hard drive

---
+++ R/copy_to_hd.R
@@ -0,0 +1,35 @@
+#' Copy comrad files to hard drive
+#'
+#' @param job_id eight-digit job ID given by Peregrine upon submission.
+#' @author Theo Pannetier
+#' @export
+#'
+copy_sim_to_hd <- function (job_ids) {
+
+  paths <- job_ids %>% path_to_sim_local()
+  new_paths <- glue::glue("F:/comrad_data/sims/comrad_sim_{job_ids}.csv")
+
+  fs::file_copy(
+    path = paths,
+    new_path = new_paths,
+    overwrite = overwrite
+  )
+  return(0)
+}
+
+#' @param job_id eight-digit job ID given by Peregrine upon submission.
+#' @author Theo Pannetier
+#' @export
+#'
+copy_sim_to_hd <- function (job_ids) {
+
+  paths <- job_ids %>% path_to_log_local()
+  new_paths <- glue::glue("F:/comrad_data/logs/comrad_sim_{job_ids}.log")
+
+  fs::file_copy(
+    path = paths,
+    new_path = new_paths,
+    overwrite = overwrite
+  )
+  return(0)
+}
