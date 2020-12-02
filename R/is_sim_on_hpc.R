bcc212061c66dad686387c8a1c5c65ed53a29632
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-12-02 15:29:29 +0100

    detect if sim file is on HPC

---
+++ R/is_sim_on_hpc.R
@@ -0,0 +1,70 @@
+#' Fetch and print simulation log from Peregrine
+#'
+#' Given a job ID, prints the corresponding `.log` file from Peregrine.
+#'
+#' @param job_id eight-digit job ID given by Peregrine upon
+#' submission.
+#'
+#' @author Théo Pannetier
+#'
+#' @name is_sim_on_hpc
+NULL
+
+#' @export
+#' @rdname cat_sim_hpc
+is_sim_csv_on_hpc <- function(
+  job_id
+) {
+  # Connect to hpc
+  session <- ssh::ssh_connect(
+    "p282688@peregrine.hpc.rug.nl"
+  )
+
+  commands <- glue::glue(
+    "test -f ", path_to_fabrika_hpc(), "comrad_data/sims/comrad_sim_{job_id}.csv"
+  )
+
+  is_on_hpc <- commands %>% map_int(function(command){
+    ssh::ssh_exec_wait(
+      session = session,
+      command = command
+    )
+  }) %>%
+    as.logical() %>%
+    magrittr::not()
+
+  # Disconnect
+  ssh::ssh_disconnect(
+    session = session
+  )
+  return(is_on_hpc)
+}
+
+#' @export
+#' @rdname cat_sim_hpc
+is_sim_log_on_hpc <- function(
+  job_id
+) {
+  # Connect to hpc
+  session <- ssh::ssh_connect(
+    "p282688@peregrine.hpc.rug.nl"
+  )
+
+  commands <- glue::glue(
+    "test -f ", path_to_fabrika_hpc(), "comrad_data/logs/comrad_sim_{job_id}.log"
+  )
+
+  is_on_hpc <- commands %>% map_int(function(command){
+    ssh::ssh_exec_wait(
+      session = session,
+      command = command
+    )
+  }) %>%
+    as.logical() %>%
+    magrittr::not()
+
+  # Disconnect
+  ssh::ssh_disconnect(
+    session = session
+  )
+}
