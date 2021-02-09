b7f7b3d4618681c20ff3a810b3dd9cdc72f882db
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-02-09 20:03:57 +0100

    fix regexp

--- R/compare_comrad_versions.R
+++ R/compare_comrad_versions.R
@@ -19,7 +19,7 @@ compare_comrad_versions <- function() {
       command = "module load R; Rscript -e \"packageVersion('comrad')\""
     )
   )[1] %>%
-    stringr::str_sub(6, 10)
+    stringr::str_sub(6, -2)
   if (!local_version == hpc_version) {
     stop("comrad versions are different between HPC and local computer")
   }
