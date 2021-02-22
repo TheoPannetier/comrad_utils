1d2f6e5ab4c868aa54d8f46d9f8206fa5a6411c6
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-02-22 10:30:24 +0100

    add sampling_on_event arg

--- R/read_mle_tbl.R
+++ R/read_mle_tbl.R
@@ -6,13 +6,16 @@
 #'
 #' @param siga parameter `competition_sd` of `comrad`
 #' @param sigk parameter `carrying_cap_sd` of `comrad`
+#' @param sampling_on_event logical, read results for
+#' `sampling_on_event = TRUE` or `sampling_on_event = FALSE` ?
 #'
 #' @export
-read_mle_tbl <- function(siga, sigk) {
+read_mle_tbl <- function(siga, sigk, sampling_on_event = TRUE) {
+  suffix <- ifelse(sampling_on_event, "full", "freq")
   dd_models <- c("lc", "xc", "ll", "lx", "xl", "xx")
   ml_tbl <- purrr::map_dfr(dd_models, function(dd_model) {
     readRDS(
-      glue::glue(path_to_fabrika_local(), "comrad_data/ml_results/ml_{dd_model}_sigk_{sigk}_siga_{siga}.rds")
+      glue::glue(path_to_fabrika_local(), "comrad_data/ml_results/ml_{dd_model}_sigk_{sigk}_siga_{siga}_{suffix}.rds")
     ) %>%
       dplyr::mutate(
         "dd_model" = dd_model,
