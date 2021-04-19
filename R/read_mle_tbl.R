d975b0b671fcff382d6f8c137740aa66eb5aa89e
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-04-19 16:12:17 +0200

    read_mle_tbl now opens to any suffix

--- R/read_mle_tbl.R
+++ R/read_mle_tbl.R
@@ -6,20 +6,19 @@
 #'
 #' @param siga parameter `competition_sd` of `comrad`
 #' @param sigk parameter `carrying_cap_sd` of `comrad`
-#' @param sampling_on_event logical, read results for
-#' `sampling_on_event = TRUE` or `sampling_on_event = FALSE` ?
+#' @param suffix character, a suffix at the end of the input files, e.g. `full`,
+#' `freq` or `median`
 #'
 #' @export
-read_mle_tbl <- function(siga, sigk, sampling_on_event = TRUE) {
-  suffix <- ifelse(sampling_on_event, "full", "freq")
-  dd_models <- c("lc", "xc", "ll", "lx", "xl", "xx")
+read_mle_tbl <- function(siga, sigk, suffix = "full") {
+  dd_models <- dd_model_names()
   ml_tbl <- purrr::map_dfr(dd_models, function(dd_model) {
     readRDS(
       glue::glue(path_to_fabrika_local(), "comrad_data/ml_results/ml_{dd_model}_sigk_{sigk}_siga_{siga}_{suffix}.rds")
     ) %>%
       dplyr::mutate(
         "dd_model" = dd_model,
-        "nb_params" = ifelse(dd_model %in% c("ll", "lx", "xx", "xl"), 4, 3),
+        "nb_params" = ifelse(stringr::str_detect(dd_model, "c"), 3, 4),
         "aic" = 2 * nb_params - 2 * loglik
       )
   })
