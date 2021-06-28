95a33e276bdf94757139770cd2c786b12fd7e342
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-06-28 21:40:34 +0200

    renamed for consistency

--- R/read_ml_with_fossil.R
+++ R/read_ml_with_fossil.R
@@ -8,15 +8,14 @@
 #' `logbook_dd_ml_with_fossil.csv`.
 #'
 #' @export
-read_ml_tbl_with_fossil <- function(job_ids) {
+read_ml_with_fossil <- function(job_ids) {
   ml_tbl <- job_ids %>% purrr::map_dfr(function(job_id) {
     readRDS(glue::glue(
-      "comrad_data/ml_results/dd_ml_with_fossil_{job_id}.rds"
+      path_to_fabrika_local(), "comrad_data/ml_results/dd_ml_with_fossil_{job_id}.rds"
     ))
   }) %>% dplyr::mutate(
     "nb_params" = ifelse(stringr::str_detect(dd_model, "c"), 3, 4),
     "aic" = 2 * nb_params - 2 * loglik
   )
-  return(ml_tbl)
   return(ml_tbl)
 }
---
+++ R/read_ml_without_fossil.R
@@ -0,0 +1,34 @@
+#' Read and combine `dd_ML` results
+#'
+#' Read all `.rds` files corresponding to `job_ids`, combine them and adds
+#' AIC scores.
+#'
+#' @param job_ids 8-digit codes (chr or num) corresponding to the ID of a
+#' `dd_ML` job, should be one of the entries in
+#' `logbook_dd_ml_without_fossil.csv`.
+#'
+#' @author Theo Pannetier
+#' @export
+read_ml_without_fossil <- function(job_ids) {
+  ml_tbl <- job_ids %>% purrr::map_dfr(function(job_id) {
+    readRDS(glue::glue(
+      path_to_fabrika_local(), "comrad_data/ml_results/dd_ml_without_fossil_{job_id}.rds"
+    ))
+  }) %>%
+    dplyr::rename(
+      "ml_lambda_0" = ml_lambda,
+      "ml_mu_0" = ml_mu,
+    ) %>%
+    dplyr::mutate(
+      # Catch failed ML results
+      "ml_lambda_0" = ifelse(loglik == -1, NA, ml_lambda_0),
+      "ml_mu_0" = ifelse(loglik == -1, NA, ml_mu_0),
+      "ml_k" = ifelse(loglik == -1, NA, ml_k),
+      "ml_alpha" = ifelse(loglik == -1, NA, ml_alpha),
+      "loglik" = ifelse(loglik == -1, -Inf, loglik),
+      # Compute AIC
+      "nb_params" = ifelse(stringr::str_detect(dd_model, "c"), 3, 4),
+      "aic" = 2 * nb_params - 2 * loglik
+    )
+  return(ml_tbl)
+}
