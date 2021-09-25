3091cc16f2fef2902cafd9e661586d181c8c7352
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-09-25 15:27:28 +0200

    utility to return which params are used for DD analysis

--- R/comrad_params_grid.R
+++ R/comrad_params_grid.R
@@ -7,3 +7,17 @@ comrad_params_grid <- function() {
     "siga" = seq(0.1, 1, 0.1)
   )
 }
+
+#' @export
+is_params_retained <- function() {
+  # retained if k_eq >= 8
+  comrad_params_grid() %>%
+    dplyr::mutate("is_retained" = c(
+      # 0.1    0.2    0.3   0.4    0.5    0.6    0.7    0.8    0.9    1
+      TRUE,  TRUE,  TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,# 1
+      TRUE,  TRUE,  TRUE, TRUE,  TRUE,  FALSE, FALSE, FALSE, FALSE, FALSE,# 2
+      TRUE,  TRUE,  TRUE, TRUE,  TRUE,  TRUE,  TRUE,  FALSE, FALSE, FALSE,# 3
+      TRUE,  TRUE,  TRUE, TRUE,  TRUE,  TRUE,  TRUE,  TRUE,  FALSE, FALSE,# 4
+      TRUE,  TRUE,  TRUE, TRUE,  TRUE,  TRUE,  TRUE,  TRUE,  TRUE,  FALSE # 5
+    ))
+}
