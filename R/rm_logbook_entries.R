65bbcb1a6edfb871ff8b5ac721186e0bf5189e6d
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-12-02 16:49:45 +0100

    housekeeping; rm'd failed jobs

---
+++ R/rm_logbook_entries.R
@@ -0,0 +1,37 @@
+rm_logbook_entries <- function(
+  job_ids,
+  which_one = "sims"
+) {
+  session <- ssh::ssh_connect(
+    "p282688@peregrine.hpc.rug.nl"
+  )
+
+  if (which_one == "sims") {
+    rel_path_to_logbook <- "comrad_data/logs/logbook.csv"
+  } else if (which_one == "dd_ml_without_fossil") {
+    rel_path_to_logbook <- "comrad_data/logs/logbook_dd_ml_without_fossil.csv"
+  } else if (which_one == "dd_ml_with_fossil") {
+    rel_path_to_logbook <- "comrad_data/logs/logbook_dd_ml_with_fossil.csv"
+  } else if (which_one == "dd_ml_with_fossil2") {
+    rel_path_to_logbook <- "comrad_data/logs/logbook_dd_ml_with_fossil2.csv"
+  } else {
+    stop("which_one should only be \"sims\", \"dd_ml_with_fossil\" or \"dd_ml_without_fossil\"")
+  }
+
+  download_logbook_hpc(which_one = which_one)
+  logbook <- read_logbook(which_one = which_one)
+
+  logbook <- logbook %>%
+    dplyr::filter(!job_id %in% job_ids)
+
+  readr::write_csv(
+    logbook,
+    file = paste0(path_to_fabrika_local(), rel_path_to_logbook)
+  )
+  ssh::scp_upload(
+    session = session,
+    files = paste0(path_to_fabrika_local(), rel_path_to_logbook),
+    to = paste0(path_to_fabrika_hpc(), "comrad_data/logs/")
+  )
+  ssh::ssh_disconnect(session = session)
+}
